b16a3cbaa778695d42878bbafc12d210f5c89bf0
Christian Russell <penguinchao@ymail.com>
2015-05-20 10:18:09 -0700

    Added Basic Pointers

---
+++ src/basic-pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x = 7
+	int* ptr = &x;
+	cout<<*ptr;
+	cout<<ptr
+	//////////////////////////////////////////////////
+	int variable = 5;
+	int* pointer = &variable;
+	cout << variable << endl
+		 << pointer << endl
+		 << *pointer << endl;
+}
